9c5f603ab201a6e11f5a7042953de0049c7d127f
Artur Furmańczyk <a.furmanczyk@smartmedia.com.pl>
2017-05-30 18:11:28 +0200

    Fixed displaying user name in edit permission view

--- DACManager/Controllers/PermissionsController.cs
+++ DACManager/Controllers/PermissionsController.cs
@@ -10,152 +10,152 @@ using DACManager.Models;
 
 namespace DACManager.Controllers
 {
-    public class PermissionsController : Controller
-    {
-        private readonly ApplicationDbContext _context;
+	public class PermissionsController : Controller
+	{
+		private readonly ApplicationDbContext _context;
 
-        public PermissionsController(ApplicationDbContext context)
-        {
-            _context = context;    
-        }
+		public PermissionsController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Permissions
-        public async Task<IActionResult> Index()
-        {
-            var applicationDbContext = _context.Permissions.Include(p => p.User);
-            return View(await applicationDbContext.ToListAsync());
-        }
+		// GET: Permissions
+		public async Task<IActionResult> Index()
+		{
+			var applicationDbContext = _context.Permissions.Include(p => p.User);
+			return View(await applicationDbContext.ToListAsync());
+		}
 
-        // GET: Permissions/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Permissions/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var permission = await _context.Permissions
-                .Include(p => p.User)
-                .SingleOrDefaultAsync(m => m.Id == id);
-            if (permission == null)
-            {
-                return NotFound();
-            }
+			var permission = await _context.Permissions
+				.Include(p => p.User)
+				.SingleOrDefaultAsync(m => m.Id == id);
+			if (permission == null)
+			{
+				return NotFound();
+			}
 
-            return View(permission);
-        }
+			return View(permission);
+		}
 
-        // GET: Permissions/Create
-        public IActionResult Create()
-        {
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
-            return View();
-        }
+		// GET: Permissions/Create
+		public IActionResult Create()
+		{
+			ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName");
+			return View();
+		}
 
-        // POST: Permissions/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UserId,Actors,Movies,Categories,Customers,Inventories,Languages,Payments,Permissions,Rentals,Staff,Stores,Receive,Transfer,LastUpdate")] Permission permission)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(permission);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName", permission.UserId);
-            return View(permission);
-        }
+		// POST: Permissions/Create
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,UserId,Actors,Movies,Categories,Customers,Inventories,Languages,Payments,Permissions,Rentals,Staff,Stores,Receive,Transfer,LastUpdate")] Permission permission)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(permission);
+				await _context.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName", permission.UserId);
+			return View(permission);
+		}
 
-        // GET: Permissions/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Permissions/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var permission = await _context.Permissions.SingleOrDefaultAsync(m => m.Id == id);
-            if (permission == null)
-            {
-                return NotFound();
-            }
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName", permission.UserId);
-            return View(permission);
-        }
+			var permission = await _context.Permissions.Include(p => p.User).SingleOrDefaultAsync(p => p.Id == id);
+			if (permission == null)
+			{
+				return NotFound();
+			}
+			ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName", permission.UserId);
+			return View(permission);
+		}
 
-        // POST: Permissions/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,Actors,Movies,Categories,Customers,Inventories,Languages,Payments,Permissions,Rentals,Staff,Stores,Receive,Transfer,LastUpdate")] Permission permission)
-        {
-            if (id != permission.Id)
-            {
-                return NotFound();
-            }
+		// POST: Permissions/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,User,Actors,Movies,Categories,Customers,Inventories,Languages,Payments,Permissions,Rentals,Staff,Stores,Receive,Transfer,LastUpdate")] Permission permission)
+		{
+			if (id != permission.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
+			if (ModelState.IsValid)
+			{
+				try
+				{
 					permission.LastUpdate = DateTime.Now;
-                    _context.Update(permission);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PermissionExists(permission.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Index");
-            }
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName", permission.UserId);
-            return View(permission);
-        }
+					_context.Update(permission);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!PermissionExists(permission.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction("Index");
+			}
+			ViewData["UserId"] = new SelectList(_context.Users, "Id", "UserName", permission.UserId);
+			return View(permission);
+		}
 
-        // GET: Permissions/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+		// GET: Permissions/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
 
-            var permission = await _context.Permissions
-                .Include(p => p.User)
-                .SingleOrDefaultAsync(m => m.Id == id);
-            if (permission == null)
-            {
-                return NotFound();
-            }
+			var permission = await _context.Permissions
+				.Include(p => p.User)
+				.SingleOrDefaultAsync(m => m.Id == id);
+			if (permission == null)
+			{
+				return NotFound();
+			}
 
-            return View(permission);
-        }
+			return View(permission);
+		}
 
-        // POST: Permissions/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var permission = await _context.Permissions.SingleOrDefaultAsync(m => m.Id == id);
-            _context.Permissions.Remove(permission);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
+		// POST: Permissions/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var permission = await _context.Permissions.SingleOrDefaultAsync(m => m.Id == id);
+			_context.Permissions.Remove(permission);
+			await _context.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
 
-        private bool PermissionExists(int id)
-        {
-            return _context.Permissions.Any(e => e.Id == id);
-        }
-    }
+		private bool PermissionExists(int id)
+		{
+			return _context.Permissions.Any(e => e.Id == id);
+		}
+	}
 }
